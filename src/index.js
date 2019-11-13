8755e7467f66f29804543616cbdb56877b5c5b6f
Dhruvam Thakkar <dhruvam.thakkar@peaas.co>
2019-11-13 15:52:02 +0530

    my app commit

--- src/index.js
+++ src/index.js
@@ -4,8 +4,27 @@ import './index.css';
 import App from './App';
 import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
+import './style.css'
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+const user = {
+    firstName: 'Code',
+    lastName: 'Flix'
+  };
+  
+const formatName = (user) => {
+    if(user){
+    return <div className="message">
+        <h1>Hello {user.firstName}</h1>
+    </div>;
+    }
+    else{
+        return "Specify Username";
+    }
+}
+
+const element = <h1>Hello, {formatName(user)}</h1>;
+
+ReactDOM.render(element, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
