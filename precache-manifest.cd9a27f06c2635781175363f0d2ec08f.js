a8bfb264c8894b89b5c5613d0f56d21eceacf3bb
Dhruvam Thakkar <dhruvam.thakkar@peaas.co>
2019-11-13 16:00:58 +0530

    Updates

---
+++ precache-manifest.cd9a27f06c2635781175363f0d2ec08f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f888463d2c059136190fe4de79f1a0ee",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "6c0acff7e4bef5556213",
+    "url": "/myapp/static/css/main.019845ac.chunk.css"
+  },
+  {
+    "revision": "07f9361d50ce0554af0d",
+    "url": "/myapp/static/js/2.b5985af0.chunk.js"
+  },
+  {
+    "revision": "6c0acff7e4bef5556213",
+    "url": "/myapp/static/js/main.bb14e747.chunk.js"
+  },
+  {
+    "revision": "27343e7b7cdb70592f08",
+    "url": "/myapp/static/js/runtime-main.f1bd5847.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/myapp/static/media/logo.25bf045c.svg"
+  }
+]);
